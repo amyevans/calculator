c37033f1a0627e9687b83a24e86ba69f8d172ad4
Amy Evans <amyevans87@gmail.com>
2015-11-02 14:38:03 -0600

    Handle decimal point, % sign, display of larger nums

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ $(document).ready(function() {
 			$output = $('.output'),
 			answer = null;
 
-	// TO DO:
-	// 1. figure out how to treat . (decimal) - allow only once; 
-	// 2. % percentage sign
-	// 3. limit display to X digits
-
 	// when 0-9 button clicked
 	$('.num').on('click', function() {
 		input = $(this).html();
-		// if operator not yet assigned, concatenate input onto 'left'
+		// if operator not yet assigned, concatenate input onto 'left'.
 		if (operator === null) {
+			// Do not allow multiple decimal points to be entered
+			if (left.indexOf('.') !== -1 && input == '.') {
+				return;
+			}
 			left = left.concat(input);
 			display(left);
 			// also, clear the answer if still set from a previous calculation
 			answer = null;
 		// since operator is assigned, concatenate input onto 'right'
 		} else {
+			// Do not allow multiple decimal points to be entered
+			if (right.indexOf('.') !== -1 && input == '.') {
+				return;
+			}
 			// if answer has a value, and the operator is clicked, then the user is trying to use the answer as their left input
 			if (answer !== null) {
 				left = answer;
@@ -37,7 +40,6 @@ $(document).ready(function() {
 		if (left && right && operator) {
 			compute();
 			left = answer;
-			right = '';
 		}
 		operator = $(this).html();
 	});
@@ -51,13 +53,29 @@ $(document).ready(function() {
 		$output.html(0);
 	});
 
+	// when percent sign clicked
+	$('.percent').on('click', function() {
+		if (right) {
+			left = +left / 100;
+			right = +right / 100;
+			compute();
+		} else if (left) {
+			left = +left / 100;
+			left = String(left);
+			display(left);
+		} else {
+			answer = answer / 100;
+			display(answer);
+			// reset
+			operator = null;
+			left = '';
+			right = '';
+		}
+	});
+
 	// when equals clicked
 	$('.equals').on('click', function() {
 		compute();
-		// reset
-		operator = null;
-		left = '';
-		right = '';
 	});
 
 	// convert left and right to numbers, evaluate operator, compute answer & display
@@ -79,11 +97,17 @@ $(document).ready(function() {
 				break;
 		}
 		display(answer);
+		// reset
+		operator = null;
+		left = '';
+		right = '';
 	}
 
 	// update display on output screen
 	function display(value) {
+		if (String(value).length > 10) {
+			value = Number(value).toPrecision(7);
+		}
 		$output.html(value);
 	}
-
 });
